1b6449424c31d76d3814370ef525e4cda979c4cb
Szymon Samuel Zborowski <zborowskis@student.mini.pw.edu.pl>
2018-12-01 23:54:50 +0100

    Day 1 finished

---
+++ Advent_2018/Advent_2018/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_2018
+{
+    class Program
+    {
+        static public string filesPath = Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory)).FullName).FullName + @"\inputs";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Day 1 - Part 1: {Day1.Day1Part1()}");
+            Console.WriteLine($"Day 1 - Part 2: {Day1.Day1Part2()}");
+        }
+    }
+}
