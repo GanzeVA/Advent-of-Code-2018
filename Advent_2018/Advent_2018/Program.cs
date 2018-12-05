a4063969f73e7ef5412640570b56730f88ded2a1
Szymon Samuel Zborowski <zborowskis@student.mini.pw.edu.pl>
2018-12-06 00:36:16 +0100

    Day 5 finished

--- Advent_2018/Advent_2018/Program.cs
+++ Advent_2018/Advent_2018/Program.cs
@@ -19,8 +19,10 @@ namespace Advent_2018
             //Console.WriteLine($"Day 2 - Part 2: {Day2.Day2Part2()}");
             //Console.WriteLine($"Day 3 - Part 1: {Day3.Day3Part1()}");
             //Console.WriteLine($"Day 3 - Part 2: {Day3.Day3Part2()}");
-            Console.WriteLine($"Day 4 - Part 1: {Day4.Day4Part1()}");
-            Console.WriteLine($"Day 4 - Part 2: {Day4.Day4Part2()}");
+            //Console.WriteLine($"Day 4 - Part 1: {Day4.Day4Part1()}");
+            //Console.WriteLine($"Day 4 - Part 2: {Day4.Day4Part2()}");
+            Console.WriteLine($"Day 5 - Part 1: {Day5.Day5Part1()}");
+            Console.WriteLine($"Day 5 - Part 2: {Day5.Day5Part2()}");
         }
     }
 }
